7480527690e55ee090351386ad43a29dd14cf463
Jorge Rodriguez <jarf4321@gmail.com>
2024-04-29 21:20:28 -0600

    [Add] solution for the class challenge

--- Semana01/clase1.js
+++ Semana01/clase1.js
@@ -6,8 +6,17 @@ function iniciarJuego() {
     // Saludando al usuario
     alert("Bienvenido al juego Piedra🗿, Papel 📃 o Tijera✂️ de Frontend II")
 
-    // Pedir el nombre al usuario y guardarlo en una variable
-    let nombre = prompt("Ingrese su nombre por favor.")
+    let nombre; // Declaramos variable para el nombre
+
+    do {
+      nombre = prompt("Ingrese su nombre por favor:").trim();
+
+      if (!nombre || nombre.length < 3) {
+        alert("Error: El nombre debe tener al menos 3 caracteres.");
+      }
+    } while (!nombre || nombre.length < 3);
+
+    nombre = nombre.toUpperCase();
 
     //    alert("Gracias por jugar " + nombre + ", ¡Mucha suerte!🍀")
     alert(`Gracias por jugar ${nombre}, ¡Mucha suerte!🍀`)
@@ -17,7 +26,6 @@ function iniciarJuego() {
     console.log("El jugador es:")
     console.log(nombre)
     console.log("--------------")
-    
 
     return nombre
 }
@@ -32,8 +40,3 @@ console.log(isNaN(nombreJugador));
 // 1- Modificar la funcion de iniciarJuego(), validar si ingresa un dato válido como nombre.
 // 2- Si no ingresa un texto, o tiene menos de 3 caracteres debemos volverle a pedir que lo ingrese.
 // 3- Finalmente el nombre devuelto debe estar todo en mayúsculas.
-
-
-
-
-
